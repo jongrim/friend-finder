9fe5ec8d13f018d352cc45deab2c15798fd0ccc0
Jonathan Grim <jonjongrim@gmail.com>
2017-08-04 15:47:47 -0400

    Update routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
-const friends = require('../api/friends/friendsRoutes.js');
+const friend = require('../api/friend/friendRoutes');
+const question = require('../api/question/questionRoutes');
 const survey = require('./survey');
 
 router.route('/').get(function(req, res) {
   res.render('index');
 });
 
-router.use('/api/friends', friends);
+router.use('/api/friend', friend);
+router.use('/api/question', question);
 router.use('/survey', survey);
 
 module.exports = router;
